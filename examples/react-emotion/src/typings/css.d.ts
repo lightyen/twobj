da003feb5ce0aeaea95ad973c046c09e9418babe
lightyen <lightyen0123@gmail.com>
2022-08-19 22:57:04 +0800

    update

--- examples/react-emotion/src/typings/css.d.ts
+++ examples/react-emotion/src/typings/css.d.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line spaced-comment
-/// <reference types="twobj/types/tw-prop" />
+/// <reference types="@emotion/react/types/css-prop" />
--- examples/react-emotion/src/typings/tw.d.ts
+++ examples/react-emotion/src/typings/tw.d.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line spaced-comment
-/// <reference types="@emotion/react/types/css-prop" />
+/// <reference types="twobj/types/tw-prop" />
