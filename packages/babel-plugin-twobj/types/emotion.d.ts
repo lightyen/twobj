d28426c9f23366c34d9d50034a4b21ef6265034b
lightyen <lightyen0123@gmail.com>
2022-08-24 19:28:43 +0800

    types

---
+++ packages/babel-plugin-twobj/types/emotion.d.ts
@@ -0,0 +1,175 @@
+declare module "twobj" {
+	import { CSSObject, PropsOf, Theme } from "@emotion/react"
+	import { Interpolation } from "@emotion/serialize"
+	import { StyledComponent, StyledOptions } from "@emotion/styled"
+	import { FilteringStyledOptions } from "@emotion/styled/types/base"
+
+	/**
+	 * @typeparam ComponentProps  Props which will be included when withComponent is called
+	 * @typeparam SpecificComponentProps  Props which will *not* be included when withComponent is called
+	 */
+	export interface CreateStyledComponent<
+		ComponentProps extends {},
+		SpecificComponentProps extends {} = {},
+		JSXProps extends {} = {},
+	> {
+		/**
+		 * @typeparam AdditionalProps  Additional props to add to your styled component
+		 */
+		<AdditionalProps extends {} = {}>(
+			...styles: Array<
+				Interpolation<ComponentProps & SpecificComponentProps & AdditionalProps & { theme: Theme }>
+			>
+		): StyledComponent<ComponentProps & AdditionalProps, SpecificComponentProps, JSXProps>
+
+		(template: TemplateStringsArray): StyledComponent<ComponentProps, SpecificComponentProps, JSXProps>
+
+		/**
+		 * @typeparam AdditionalProps  Additional props to add to your styled component
+		 */
+		<AdditionalProps extends {}>(template: TemplateStringsArray): StyledComponent<
+			ComponentProps & AdditionalProps,
+			SpecificComponentProps,
+			JSXProps
+		>
+	}
+
+	export interface CreateStyled {
+		/**
+		 * @desc
+		 * This function accepts a React component or tag ('div', 'a' etc).
+		 *
+		 * @example tw(MyComponent)`w-full`
+		 * @example tw(MyComponent)(myComponentProps => ({ width: myComponentProps.width })
+		 * @example tw('div')`w-full`
+		 * @example tw('div')<Props>(props => ({ width: props.width })
+		 */
+		<
+			C extends React.ComponentClass<React.ComponentProps<C>>,
+			ForwardedProps extends keyof React.ComponentProps<C> = keyof React.ComponentProps<C>,
+		>(
+			component: C,
+			options: FilteringStyledOptions<React.ComponentProps<C>, ForwardedProps>,
+		): CreateStyledComponent<
+			Pick<PropsOf<C>, ForwardedProps> & {
+				theme?: Theme
+			},
+			{},
+			{
+				ref?: React.Ref<InstanceType<C>>
+			}
+		>
+
+		/**
+		 * @desc
+		 * This function accepts a React component or tag ('div', 'a' etc).
+		 *
+		 * @example tw(MyComponent)`w-full`
+		 * @example tw(MyComponent)(myComponentProps => ({ width: myComponentProps.width })
+		 * @example tw('div')`w-full`
+		 * @example tw('div')<Props>(props => ({ width: props.width })
+		 */
+		<C extends React.ComponentClass<React.ComponentProps<C>>>(
+			component: C,
+			options?: StyledOptions<React.ComponentProps<C>>,
+		): CreateStyledComponent<
+			PropsOf<C> & {
+				theme?: Theme
+			},
+			{},
+			{
+				ref?: React.Ref<InstanceType<C>>
+			}
+		>
+
+		/**
+		 * @desc
+		 * This function accepts a React component or tag ('div', 'a' etc).
+		 *
+		 * @example tw(MyComponent)`w-full`
+		 * @example tw(MyComponent)(myComponentProps => ({ width: myComponentProps.width })
+		 * @example tw('div')`w-full`
+		 * @example tw('div')<Props>(props => ({ width: props.width })
+		 */
+		<
+			C extends React.ComponentType<React.ComponentProps<C>>,
+			ForwardedProps extends keyof React.ComponentProps<C> = keyof React.ComponentProps<C>,
+		>(
+			component: C,
+			options: FilteringStyledOptions<React.ComponentProps<C>, ForwardedProps>,
+		): CreateStyledComponent<
+			Pick<PropsOf<C>, ForwardedProps> & {
+				theme?: Theme
+			}
+		>
+
+		/**
+		 * @desc
+		 * This function accepts a React component or tag ('div', 'a' etc).
+		 *
+		 * @example tw(MyComponent)`w-full`
+		 * @example tw(MyComponent)(myComponentProps => ({ width: myComponentProps.width })
+		 * @example tw('div')`w-full`
+		 * @example tw('div')<Props>(props => ({ width: props.width })
+		 */
+		<C extends React.ComponentType<React.ComponentProps<C>>>(
+			component: C,
+			options?: StyledOptions<React.ComponentProps<C>>,
+		): CreateStyledComponent<
+			PropsOf<C> & {
+				theme?: Theme
+			}
+		>
+
+		/**
+		 * @desc
+		 * This function accepts a React component or tag ('div', 'a' etc).
+		 *
+		 * @example tw(MyComponent)`w-full`
+		 * @example tw(MyComponent)(myComponentProps => ({ width: myComponentProps.width })
+		 * @example tw('div')`w-full`
+		 * @example tw('div')<Props>(props => ({ width: props.width })
+		 */
+		<
+			Tag extends keyof JSX.IntrinsicElements,
+			ForwardedProps extends keyof JSX.IntrinsicElements[Tag] = keyof JSX.IntrinsicElements[Tag],
+		>(
+			tag: Tag,
+			options: FilteringStyledOptions<JSX.IntrinsicElements[Tag], ForwardedProps>,
+		): CreateStyledComponent<
+			{ theme?: Theme; as?: React.ElementType },
+			Pick<JSX.IntrinsicElements[Tag], ForwardedProps>
+		>
+
+		/**
+		 * @desc
+		 * This function accepts a React component or tag ('div', 'a' etc).
+		 *
+		 * @example tw(MyComponent)`w-full`
+		 * @example tw(MyComponent)(myComponentProps => ({ width: myComponentProps.width })
+		 * @example tw('div')`w-full`
+		 * @example tw('div')<Props>(props => ({ width: props.width })
+		 */
+		<Tag extends keyof JSX.IntrinsicElements>(
+			tag: Tag,
+			options?: StyledOptions<JSX.IntrinsicElements[Tag]>,
+		): CreateStyledComponent<{ theme?: Theme; as?: React.ElementType }, JSX.IntrinsicElements[Tag]>
+	}
+
+	/** twin.macro */
+	export type StyledTags = {
+		[Tag in keyof JSX.IntrinsicElements]: CreateStyledComponent<
+			{
+				theme?: Theme
+				as?: React.ElementType
+			},
+			JSX.IntrinsicElements[Tag]
+		>
+	}
+
+	interface Tw extends StyledTags, CreateStyled {
+		(arr: TemplateStringsArray): CSSObject
+	}
+
+	export const tw: Tw
+}
