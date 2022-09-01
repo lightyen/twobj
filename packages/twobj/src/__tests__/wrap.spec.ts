0ebe161c7bee56f2dcf9fb2477242f626427af84
lightyen <lightyen0123@gmail.com>
2022-09-01 15:06:31 +0800

    fix wrap function

---
+++ packages/twobj/src/__tests__/wrap.spec.ts
@@ -0,0 +1,26 @@
+import { tw } from "./context"
+
+test("wrap function", () => {
+	expect(tw`hover:$e`).toEqual({
+		"@media (hover: hover) and (pointer: fine)": {
+			"&:hover": Math.E,
+		},
+	})
+	expect(tw`sm:hover:$e`).toEqual({
+		"@media (min-width: 640px)": {
+			"@media (hover: hover) and (pointer: fine)": {
+				"&:hover": Math.E,
+			},
+		},
+	})
+	expect(tw`sm:hover:marker:$e`).toEqual({
+		"@media (min-width: 640px)": {
+			"@media (hover: hover) and (pointer: fine)": {
+				"&:hover": {
+					"& *::marker": Math.E,
+					"&::marker": Math.E,
+				},
+			},
+		},
+	})
+})
