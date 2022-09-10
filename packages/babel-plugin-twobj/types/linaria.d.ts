c768e46d9dc0cac4934ef4c43bb4f5739c879547
lightyen <lightyen0123@gmail.com>
2022-09-10 17:35:17 +0800

    babel-plugin: fix types

--- packages/babel-plugin-twobj/types/linaria.d.ts
+++ packages/babel-plugin-twobj/types/linaria.d.ts
@@ -1,7 +1,9 @@
+import type { CSSProperties } from "@linaria/react"
 import {} from "react"
+
 declare module "twobj" {
-	import type { CSSProperties } from "twobj"
-	export const globalStyles: Record<string, CSSProperties>
 	export function tw(arr: TemplateStringsArray): CSSProperties
+	export const globalStyles: CSSProperties
 	export function theme(arr: TemplateStringsArray): unknown
+	export function wrap(arr: TemplateStringsArray): (arg: CSSProperties) => CSSProperties
 }
