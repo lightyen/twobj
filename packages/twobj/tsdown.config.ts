b86b34fb311fe76f4a1182ac6c261f4b4da633c8
lightyen <lightyen0123@gmail.com>
2026-07-31 19:57:21 +0800

    fix package

--- packages/twobj/tsdown.config.ts
+++ packages/twobj/tsdown.config.ts
@@ -3,15 +3,39 @@ import { defineConfig } from "tsdown"
 export default defineConfig([
 	{
 		entry: ["src/index.ts"],
-		format: ["esm"],
+		format: ["esm", "cjs"],
 		dts: {
 			tsconfig: "./tsconfig.json",
+		},
+		outExtension({ format }) {
+			if (format === "es") {
+				return {
+					js: ".mjs",
+					dts: ".d.ts",
+				}
+			}
+			return {
+				js: ".js",
+				dts: ".d.ts",
+			}
 		},
 	},
 	{
 		entry: ["src/parser/index.ts"],
 		outDir: "./parser",
 		dts: true,
-		format: ["esm"],
+		format: ["esm", "cjs"],
+		outExtension({ format }) {
+			if (format === "es") {
+				return {
+					js: ".mjs",
+					dts: ".d.ts",
+				}
+			}
+			return {
+				js: ".js",
+				dts: ".d.ts",
+			}
+		},
 	},
 ])
